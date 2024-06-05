40b6d2724ac1ecfc51deec12aae01c484d935806
Pablo <cardosopab@gmail.com>
2024-06-05 02:36:20 -0500

    Remove complement observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
-import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mentis_math/app.dart';
-import 'package:mentis_math/complement_observer.dart';
 
 void main() {
-  Bloc.observer = const ComplementObserver();
   runApp(const MentisMathApp());
 }
